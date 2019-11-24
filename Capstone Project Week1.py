1ec1f0baf01ca7deb0e7bb102b5228ff73725348
taha082 <58131635+taha082@users.noreply.github.com>
2019-11-24 15:52:49 +0530

    Add files via upload

---
+++ Capstone Project Week1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### This is mainly for the IBM Data Science Capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
